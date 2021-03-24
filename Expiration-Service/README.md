ee0944f0fac3d7a45245a506b43d1f48dedca6e1
Narendran <narendev0610@gmail.com>
2021-03-24 18:06:40 +0530

    Expiration-Service v1-beta added

---
+++ Expiration-Service/README.md
@@ -0,0 +1,3 @@
+# Expiration Service
+
+### Details (WIP)
---
+++ Expiration-Service/src/index.ts
@@ -0,0 +1,42 @@
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
+import { natsWrapper } from "./nats-wrapper";
+
+// mongoose connection
+const start = async () => {
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+
+  if (!process.env.REDIS_HOST) {
+    throw new Error("REDIS_HOST must be defined")
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    // ? Graceful shutdown for NATS streaming server
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
